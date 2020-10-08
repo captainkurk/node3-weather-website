d66ff7b421b060d043fdf6d2b2defdecfe810a75
Samuel T <samuelisabeth@outlook.com>
2020-10-08 08:54:39 -0400

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const forecast = (latitude, longitude,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c22ea1eedec13d89ef68784a804325ef&query=' + latitude + ',' + longitude + '&units=f'
+    request({url, json: true}, (error,{body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+            }
+        else if (body.error) {
+            callback('Unable to find location.',undefined)
+            }
+        else {
+            callback(undefined,{
+                temperature: body.current.temperature,
+                feelslike: body.current.feelslike
+            })
+        }
+    })
+
+}
+
+
+
+
+module.exports = forecast
